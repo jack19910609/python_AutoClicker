5779c3023912cc9a8aeddd71f5cb90490d233b2e
Jimmy <jack19910609@gmail.com>
2023-06-29 23:53:16 +0800

    修正秒數

--- AutoCliicker.py
+++ AutoCliicker.py
@@ -4,7 +4,7 @@ from pynput.mouse import Button, Controller as MouseController
 from pynput.keyboard import Listener, KeyCode, Key ,Controller as KeyboardController
 
 
-mouseDelay = 0.2
+mouseDelay = 0.15
 keyboardDelay = 0.01
 button = Button.left
 start_stop_key = KeyCode(char='s')
